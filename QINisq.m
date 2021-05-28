73e839910098ec8c2db20aee25ad4785faebbdad
Jarek Miszczak <jarek@miszczak.eu>
2021-05-28 21:20:55 +0200

    Added functions for running quantum circuits

--- QINisq.m
+++ QINisq.m
@@ -32,16 +32,22 @@ RZ::usage = "RX[theta] one-qubit rotation wrt Z axis.";
 RXP::usage = "PXR[phi, theta] one-qubit rotation of the Bloch vector by an angle theta, where phi is the angle between the rotation axis and the X axis.";
 
 
-S::usage = "S = RZ[pi/2]";
+S::usage = "S[] = RZ[pi/2]";
 
 
-T::usage = "T = RZ[pi/4]";
+T::usage = "T[] = RZ[pi/4]";
 
 
-toffoli::usage = "Toffoli/controlled-controlled-not gate.";
+H::usage = "H[] = Hadamard gate.";
+
+
+Toff::usage = "Toff[] = Toffoli/controlled-controlled-not gate.";
 
 
 XX::usage = "Ising gate with parameter \[Theta]."
+
+
+Gate::usage = "Gate[g,t,q] returns quantum gate applied on systems specified in list t, on the computer with q qubits.";
 
 
 CGate::usage = "CGate[g,c,t,q] returns controlled version of one-qubit gate g with control qubits c and target qubits q, acring on q qubits. Please note that this function does not check of the dimensions are appropriate and qubits specification do not overlap.";
@@ -50,11 +56,23 @@ CGate::usage = "CGate[g,c,t,q] returns controlled version of one-qubit gate g wi
 CX::usage = "CX[c,t,qdim] generalized controlled not operation with control qubits c and target qubits t, acting on qdim qubits. Please note that this function does not check of the dimensions are appropriate and qubits specification do not overlap.";
 
 
+CY::usage = "CX[c,t,qdim] generalized controlled sy. See CX usage info for more details."
+
+
+CZ::usage = "CX[c,t,qdim] generalized controlled sz. See CX usage info for more details."
+
+
 (* ::Subsection:: *)
 (*Quantum computer*)
 
 
-Q::usage = "Q[qg, ops, qc] appends gate qg to the computer qc using options in the ops list.";
+Q::usage = "Q[qg, ops, qc] appends gate qg to the computer qc using options in the ops list. It is expected that the first element of qc list is an integer representing the number of available qubits.";
+
+
+RunGate::usage = "RunGate[g,t,q] executes one-qubit operation operation g on qubits listed in t on the register of q qubits.";
+
+
+RunCGate::usage = "RunCGate[g,ops,q] runs controlled gate c with control and target qubits specified in ops on a system with q qubits.";
 
 
 QRun::usage = "QRun[qc] runs a simulation of the intructions in the list of gates, using the information about the paramters and the size of the computer qc.";
@@ -94,7 +112,8 @@ qiNisqHistory = {
 	{"0.0.3", "17/05/2021", "Jarek", "Added Bloch verctor rotation wrt rotatet X axis."},
 	{"0.0.4", "21/05/2021", "Jarek", "Added toffoli, S and T gates"},
 	{"0.0.5", "22/05/2021", "Jarek", "Added general controlled one qubit gate and templates for CX, CY and CZ gates."},
-	{"0.0.6", "25/05/2021", "Jarek", "Added template for function managing virtual quantum devices."}
+	{"0.0.6", "25/05/2021", "Jarek", "Added template for function managing virtual quantum devices."},
+	{"0.0.7", "28/05/2021", "Jarek", "Added function for extending one-qubit-gates and functions for executing non-parametric one-qubit gates and controlled gates."}
 };  
 
 
@@ -120,10 +139,13 @@ RY[theta_]:=MatrixExp[- I theta/2 sy];
 RZ[theta_]:=MatrixExp[- I theta/2 sz];
 
 
-S = MatrixPower[sz,1/2]
+S[] = MatrixPower[sz,1/2];
 
 
-T = MatrixPower[sz,1/4]
+T[] = MatrixPower[sz,1/4];
+
+
+H[] = 1/Sqrt[2]{{1,1},{1,-1}};
 
 
 RXP[phi_,theta_]:={ {Cos[theta/2], -I Sin[theta/2] Exp[-I phi]}, {-I Sin[theta/2] Exp[I phi], Cos[theta/2]} };
@@ -135,9 +157,16 @@ toffoli = Proj[Ket[1,2]]\[CircleTimes]Proj[Ket[1,2]]\[CircleTimes]sx + (Id[4]-Pr
 XX[theta_]:=Cos[theta](Id[2]\[CircleTimes]Id[2])-I Sin[theta](sx\[CircleTimes]sx);
 
 
+Gate[op_,targ_,qdim_]:=Block[{res},
+	res =Table[id,{qdim}];
+	res[[Flatten[{targ}]]] = Table[op,{Flatten[{targ}]}];
+	KroneckerProduct@@res
+];
+
+
 CGate[gate_,ctrl_,targ_,qdim_]:=Block[{idx,currIdx,idle},
 	idle = Complement[Range[1,qdim],ctrl~Join~targ];
-	idx=DeleteDuplicates[
+	idx = DeleteDuplicates[
 	Table[
 			currIdx=Reverse@IntegerDigits[i,2,qdim];
 			currIdx[[idle]]=Table[id,{idle}];
@@ -157,10 +186,10 @@ CGate[gate_,ctrl_,targ_,qdim_]:=Block[{idx,currIdx,idle},
 CX[c_,t_,qdim_]:=CGate[sx, Flatten[{c}], Flatten[{t}],qdim];
 
 
-CY[c_,t_,qdim_]:=CGate[sy, Flatten[{c}], Flatten[{t}],qdim]
+CY[c_,t_,qdim_]:=CGate[sy, Flatten[{c}], Flatten[{t}],qdim];
 
 
-CZ[c_,t_,qdim_]:=CGate[sz, Flatten[{c}], Flatten[{t}],qdim]
+CZ[c_,t_,qdim_]:=CGate[sz, Flatten[{c}], Flatten[{t}],qdim];
 
 
 (* ::Subsection:: *)
@@ -171,9 +200,34 @@ Q[gate_,ops_,qc_]:=AppendTo[qc,{gate,ops}];
 SetAttributes[Q,HoldAll];
 
 
-QRun[qc_,init_:{}]:=Block[{dim=qc[[1]],inState},
-inState=Ket[0,2^dim];
-inState];
+RunGate[g_,ops_,qdim_]:=ToExpression[
+	StringTemplate["Gate[`OP`[],`targ`,`qdim`]"][
+		<|"OP"->g,
+		"targ"->ops ,
+		"qdim"->qdim|>]
+];
+
+
+RunCGate[g_,ops_,qdim_]:=ToExpression[
+	StringTemplate["`OP`[`ctrl`,`targ`,`qdim`]"][
+		<|"OP"->g,
+		"ctrl"->ops[[1]] ,
+		"targ"->ops[[2]],
+		"qdim"->qdim|>]
+];
+
+
+QRun[qc_]:=Block[{dim=qc[[1]]},
+(
+	Dot@@Table[
+	Switch[
+	Head[qc[[i]][[2]][[1]]],
+		Integer,RunGate[qc[[i]][[1]],qc[[i]][[2]],qc[[1]]],
+		List,RunCGate[qc[[i]][[1]],{qc[[i]][[2,1]],qc[[i]][[2,2]]},qc[[1]]]
+		],
+	{i,2,Length[qc]}
+])
+];
 
 
 (* ::Subsection:: *)
@@ -199,3 +253,6 @@ End[] (* End Private Context *)
 Protect@@Names["QINisq`*"]
 
 EndPackage[]
+
+
+
